527a7890818e4ab5c93aa0115b4355a50d683e38
achernyshev94 <88447460+achernyshev94@users.noreply.github.com>
2023-07-02 16:48:13 +0300

    fix

--- src/test/kotlin/tests/FirstTest.kt
+++ src/test/kotlin/tests/FirstTest.kt
@@ -24,7 +24,7 @@ class FirstTest {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8")
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "and80")
         capabilities.setCapability(MobileCapabilityType.APP,
-            "/Users/alexanderchernyshev/IdeaProjects/KotlinAppium/src/test/resources/apks/org.wikipedia.apk")
+            "/Users/alexanderchernyshev/IdeaProjects/AppiumKotlinAutomation/src/test/resources/apks/org.wikipedia.apk")
 
         driver = AndroidDriver(serverAddress, capabilities)
     }
