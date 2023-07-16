ea52b94fc534dc036b63e17fbf23ef821408631e
achernyshev94 <88447460+achernyshev94@users.noreply.github.com>
2023-07-16 20:28:12 +0300

    fix assertElementPresent

--- src/test/kotlin/tests/TitleTest.kt
+++ src/test/kotlin/tests/TitleTest.kt
@@ -5,6 +5,7 @@ import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
 import io.appium.java_client.remote.MobileCapabilityType
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.openqa.selenium.By
@@ -25,7 +26,7 @@ class TitleTest {
 
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android")
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium")
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8")
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9")
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "and80")
         capabilities.setCapability(
             MobileCapabilityType.APP,
@@ -75,7 +76,8 @@ class TitleTest {
         wait.withMessage(errorMessage + "\n")
         return wait.until(ExpectedConditions.presenceOfElementLocated(by))
     }
-    private fun assertElementPresent(by: By, errorMessage: String): WebElement {
-        return waitForElementPresent(by, errorMessage, 0)
+    private fun assertElementPresent(by: By, errorMessage: String) {
+        val element = driver?.findElements(by)
+        Assert.assertTrue(errorMessage, element!!.size > 0)
     }
 }
